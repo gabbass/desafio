c108a2b19f4402fe39575ebb7af1d8d3ba64f362
gabbass <34526151+gabbass@users.noreply.github.com>
2017-12-13 23:12:44 -0200

    Add files via upload
    Criação

---
+++ criando.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE cadastro; 
+
+CREATE TABLE contatos (
+nome VARCHAR(50),
+data_D VARCHAR(50),
+email VARCHAR(50),
+racf VARCHAR(50),
+senha VARCHAR(50),
+csenha VARCHAR(50)
+
+
+);
+
+ CREATE TABLE tarefa (
+id_task VARCHAR(50) auto_increment,
+titulo VARCHAR(50),
+datai VARCHAR(50),
+dataf VARCHAR(50),
+nome_ex  VARCHAR(50),
+arquivo VARCHAR(50),
+descricao VARCHAR(50),
+
+PRIMARY KEY (id_task)
+
+);
+
+ 
